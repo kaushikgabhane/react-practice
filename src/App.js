05d6e8e3525932e383423e8090350df5b38daf2c
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-02-20 19:52:11 +0530

    practice with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function SuperHero(){
+
+  const [hero,setHero] = useState(["Shaktiman", " ironman", "hulk"])
+
+  const [name,setName] = useState( ()=> "antman")
+
+  const onAddName = () => {
+    setHero([...hero , name])
+    setName("")
+  }
+
+  return (
+    <div> 
+      <ul>
+        {hero.map((h)=> (
+          <li key={h}>{h}</li>
+        ))}
+      </ul>
+      <input 
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button
+        onClick={onAddName}
+      >Add Value</button>
+    </div>
+  )
+}
+
+function Counter(){
+  let [count , setCount] = useState(100);
+  
+  function oneUp(){
+    setCount(count + 1)
+  }
+
+  function oneDown(){
+    setCount(count - 1)
+  }
+
+  return(
+    <div> 
+      <button 
+      onClick={oneUp}
+      >+</button>
+      <h1>{count}</h1>
+      <button
+        onClick={oneDown}
+      > - </button>
+    </div>
+  )
+}
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>hello to react</h1>
+      <Counter />
+      <SuperHero />
+
+    </>
   );
 }
 
