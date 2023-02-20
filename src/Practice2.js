65f15a65e74ab2c5f0a53e6e974f4f91cbdb2d8a
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-02-20 20:47:13 +0530

    Deleted unwanted files and day 2 practice started

---
+++ src/Practice2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Practice2() {
+  return (
+    <div>Practice2</div>
+  )
+}
+
+export default Practice2
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import Practice from './Practice2'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Practice />
   </React.StrictMode>
 );
 
